0bfb30037b6e8926519c2fca54aee734778353da
Cameron Dix <cdix95@gmail.com>
2017-10-25 18:51:08 -0700

    Update Iterator.h

--- Iterator.h
+++ Iterator.h
@@ -3,12 +3,12 @@
 
 class Iterator {
 public:
+    Iterator(Link* link) : link(link) {}
     int& operator*() const;
     void operator++();
     bool operator== (const Iterator& rhs);
 
 private:
-    Iterator(Link* link) : link(link) {}
     Link* link;
     friend class List;
 };
